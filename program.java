92dbb036f6ee55e6ea46d9fc02a0f4bbf7a7dcf8
menaga <menagap43@gmail.com>
2026-07-13 21:22:39 +0530

    add this program

--- program.java
+++ program.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
 public class program {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        String name = scan.nextLine();
+        double score = scan.nextDouble();
+        scan.nextLine();
+        String department = scan.nextLine();
+        System.out.println("my name is " + name);
+        System.out.println("my score is " + score/10);
+        System.out.println("my department is " + department);
+    }
     
 }
