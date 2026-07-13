31ca401a730432f51196ac2baca723393b01571a
menaga <menagap43@gmail.com>
2026-07-13 20:34:07 +0530

    added my program

--- input.java
+++ input.java
@@ -1,10 +1,18 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 public class input{
     public static void main(String args[]){
         Scanner scan =new Scanner(System.in);
         String name =scan.nextLine();
         int age=scan.nextInt();
-        System.out.print("my name is"+name);
-        System.out.print("my age is"+age);
+        System.out.println("my name is "+ name);
+        System.out.println("my age is "+ age);
+    }
+}*/
+
+public class input{
+    public static void main(String[] args) {
+    int a =10;
+    int b=20;
+    System.out.print(a+b);
     }
 }
