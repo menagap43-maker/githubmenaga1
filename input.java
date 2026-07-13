4f18f9569649c61c2ecacdb8409746090ac824c1
menaga <menagap43@gmail.com>
2026-07-13 20:02:45 +0530

    rename text1.txt to input.java

--- input.java
+++ input.java
@@ -1,7 +1,7 @@
-import java .util.Scannner;
+import java.util.Scanner;
 public class input{
     public static void main(String args[]){
-        Scanner scan =new Scannen(System.in);
+        Scanner scan =new Scanner(System.in);
         String name =scan.nextLine();
         int age=scan.nextInt();
         System.out.print("my name is"+name);
