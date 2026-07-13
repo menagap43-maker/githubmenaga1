dad429073b1898758d11de149f315f099d09b56f
menaga <menagap43@gmail.com>
2026-07-13 20:53:18 +0530

    added my program

--- input.java
+++ input.java
@@ -9,10 +9,23 @@ public class input{
     }
 }*/
 
-public class input{
+/*public class input{
     public static void main(String[] args) {
     int a =10;
     int b=20;
     System.out.print(a+b);
     }
+}*/
+
+import java.util.Scanner;
+public class input{
+public static void main(String args[]){
+Scanner scan = new Scanner (System.in);
+int a= scan.nextInt();
+int b= scan.nextInt();
+int c= scan.nextInt();
+int d =a*b*c;
+int e =a+b+c;
+System.out.print(d/e);
+}
 }
